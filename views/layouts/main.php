b805f2c6965ccbb6ffd39d46c1500e02e9e9b540
Airat Sirazov <sirazovairat@yandex.ru>
2021-11-14 00:54:05 +0300

    feat: add logout button

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,17 +1,15 @@
 <?php
 
-/* @var $this \yii\web\View */
+/* @var $this yii\web\View */
 /* @var $content string */
 
 use app\assets\AppAsset;
-use app\widgets\Alert;
-use yii\bootstrap4\Breadcrumbs;
 use yii\bootstrap4\Html;
-use yii\bootstrap4\Nav;
 use yii\bootstrap4\NavBar;
 
 AppAsset::register($this);
 ?>
+
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>" class="h-100">
@@ -34,28 +32,13 @@ AppAsset::register($this);
             'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
         ],
     ]);
-//    echo Nav::widget([
-//        'options' => ['class' => 'navbar-nav'],
-//        'items' => [
-//            ['label' => 'Home', 'url' => ['/site/index']],
-//            ['label' => 'About', 'url' => ['/site/about']],
-//            ['label' => 'Contact', 'url' => ['/site/contact']],
-//            Yii::$app->user->isGuest ? (
-//                ['label' => 'Login', 'url' => ['/site/login']]
-//            ) : (
-//                '<li>'
-//                . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
-//                . Html::submitButton(
-//                    'Logout (' . Yii::$app->user->identity->username . ')',
-//                    ['class' => 'btn btn-link logout']
-//                )
-//                . Html::endForm()
-//                . '</li>'
-//            )
-//        ],
-//    ]);
-    NavBar::end();
     ?>
+
+    <?php if (!Yii::$app->user->isGuest) { ?>
+        <?=  Html::a('Logout', ['profile/logout']) ?>
+    <?php } ?>
+
+    <?php NavBar::end(); ?>
 </header>
 
 <main role="main" class="flex-shrink-0">
@@ -63,6 +46,7 @@ AppAsset::register($this);
         <?= $content ?>
     </div>
 </main>
+
 <?php $this->endBody() ?>
 </body>
 </html>
