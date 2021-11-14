690218c45a7a545f14cb59c61d4627960cd27ee4
Airat Sirazov <sirazovairat@yandex.ru>
2021-11-14 03:29:10 +0300

    fix: fix login validation

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -40,8 +40,8 @@ class LoginForm extends Model
         return [
             [['email', 'password'], 'required'],
             ['email', 'email'],
-            ['email', 'validateEmail'],
             ['password', 'validatePassword'],
+            ['email', 'validateEmail'],
         ];
     }
 
